c3290437df016edbe9376a323464613a0d1edd06
amimtiaz <amemtiaj19@gmail.com>
2024-02-26 13:18:37 +0600

    added prime number finder problem and solve

--- src/main/java/org/example/Practice.java
+++ src/main/java/org/example/Practice.java
@@ -1,15 +1,14 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
 
 import static java.awt.SystemColor.text;
 
 public class Practice {
     public static void main(String[] args) {
 
-        double number = 16.0;
-        double squareRoot = Math.sqrt(number);
-        System.out.println("Square root of " + number + " is: " + squareRoot);
 
     }
 }
