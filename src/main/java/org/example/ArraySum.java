d03843483ecad1f9366d05ab78f875316f257510
amimtiaz <amemtiaj19@gmail.com>
2024-02-29 17:30:21 +0600

    added array length find out

---
+++ src/main/java/org/example/ArraySum.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5};
+
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        System.out.println("The sum of the elements in the array is: " + sum);
+    }
+}
